9757b360485b7241f34f61e435ed3f09da6d5cf0
Jonny Paiva <jonny_paiva@hotmail.com.br>
2025-03-12 14:17:21 -0300

    commit

---
+++ java-anatomia-class/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+
+public static void main (String [] args) {
+
+    System.out.print ("Olá Pessoal, tudo bem?");
+    String name = System.in.nullInputStream();
+}
+    
+}
